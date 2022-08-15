2f1e1eaf22e7cc680fbabf7c4fe159695e0d904f
Caesar Reyes <caesar.reyes@citystyle.co.jp>
2022-08-15 08:06:06 +0000

    346 Creating Custom Lists Using Express Route Parametes - Still NG

--- app.js
+++ app.js
@@ -1,33 +1,128 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+// const date = require(__dirname + "/date.js");
 
 const app = express();
-
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://192.168.56.11:27017/todolistDB");
+
+const itemsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Check Entry. No name specified."],
+  },
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Clean House.",
+});
+
+const item2 = new Item({
+  name: "Clean Car.",
+});
+
+const item3 = new Item({
+  name: "Clean Garage.",
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema],
+};
+
+const List = mongoose.model("List", listSchema);
+
+// Item.insertMany(defaultItems, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Success.");
+//   }
+// });
+
 app.get("/", function (req, res) {
-  let day = date.getDate();
-  res.render("list", { listTitle: day, newListItems: items });
-  //  res.send("Hello");
+  //  let day = date.getDate();
+
+  Item.find({}, (err, foundItems) => {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Success.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+      //    console.log(foundItems);
+    }
+  });
+});
+
+app.get("/:customListName", (req, res) => {
+  const customListName = req.params.customListName;
+  console.log(customListName);
+
+  List.findOne({ name: customListName }, (err, foundList) => {
+    if (!err) {
+      if (!foundList) {
+        // Create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems,
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      }
+    } else {
+      res.render("list", {
+        listTitle: foundList.name,
+        newListItems: foundList.items,
+      });
+    }
+  });
 });
 
 app.post("/", function (req, res) {
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item({
+    name: itemName,
+  });
+  item.save();
+  res.redirect("/");
+
+  // if (req.body.list === "Work") {
+  //   workItems.push(item);
+  //   res.redirect("/work");
+  // } else {
+  //   items.push(item);
+  //   res.redirect("/");
+  // }
+});
+
+app.post("/delete", (req, res) => {
+  checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+      // console.log("Success Deleting Item.");
+    }
+  });
 });
 
 app.get("/work", function (req, res) {
